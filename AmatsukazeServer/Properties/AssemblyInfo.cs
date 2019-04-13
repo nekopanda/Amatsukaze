819b984ed3970c6a5ea27a27aa62295434f75c12
Nekopanda <pianoyayaninth@yahoo.co.jp>
2019-04-13 20:05:16 +0900

    0.9.0.0

--- AmatsukazeServer/Properties/AssemblyInfo.cs
+++ AmatsukazeServer/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.8.8.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
